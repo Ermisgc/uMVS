e167ac58c02880a84901d63b309d56f32346313f
1464817557@qq.com <1464817557@qq.com>
2025-11-10 21:57:42 +0800

    first commit

---
+++ inc/geometry/vector.h
@@ -0,0 +1,19 @@
+#ifndef U3D_GEOMETRY_VECTOR_H
+#define U3D_GEOMETRY_VECTOR_H
+#include "utils.h"
+NAMESPACE_BEGIN
+namespace geometry {
+    struct Vector3{
+        double x, y, z;
+    }; 
+
+
+    struct Vector2{
+        double x, y;
+    };
+    
+
+
+}
+NAMESPACE_END
+#endif
---
+++ inc/utils.h
@@ -0,0 +1,11 @@
+#ifndef U3D_UTILS_H
+#define U3D_UTILS_H
+
+#define NAMESPACE_U3D u3d
+#define USING_U3D using namespace NAMESPACE_U3D;
+
+#define NAMESPACE_BEGIN namespace NAMESPACE_U3D {
+#define NAMESPACE_END }
+
+
+#endif
